102e675bcc9c4f7bc5f697f4910e5a26f07eb0e6
Sam Epstein <same008@gmail.com>
2010-12-26 19:32:10 -0500

    moved routers out to seperate out api routes from others
    Signed-off-by: Sam Epstein <same008@gmail.com>

--- server/distro-server.js
+++ server/distro-server.js
@@ -39,81 +39,83 @@ global.db.open(function(err, db){
 			connect.logger(),
 			connect.cookieDecoder(),
 			connect.bodyDecoder(),
-			connect.router(function(app) {
-				function methodNotAllowed(req, res, params){
-					res.writeHead(405);
-					res.end("Method Not Allowed");
-				}
+			connect.staticProvider(__dirname + '/static')
+		)
+		.use('/api/', connect.router(function(app) {
+			function methodNotAllowed(req, res, params){
+				res.writeHead(405);
+				res.end("Method Not Allowed");
+			}
 
-				app.get('/api/login', methodNotAllowed);
-				app.post('/api/login', distro.request.handleRequest(false, function(session, req, res, successback, errback){
-					var login = req.body;
-					if(login && login.email && login.password){
-						global.users.userWithCredentials(login.email, login.password, function(err, user){
-							if(user){
-								global.sessions.startSessionForUserID(user._id, login.extendedSession, req, res, function(err){
-									if(err){
-										errback(err);
-									} else {
-										successback({userName:user.email});
-									}
-								});	
-							} else {
-								errback(new distro.error.ClientError("registration.errors.invalidCredentials"));
-							}
-						});
+			app.get('/login', methodNotAllowed);
+			app.post('/login', distro.request.handleRequest(false, function(session, req, res, successback, errback){
+				var login = req.body;
+				if(login && login.email && login.password){
+					global.users.userWithCredentials(login.email, login.password, function(err, user){
+						if(user){
+							global.sessions.startSessionForUserID(user._id, login.extendedSession, req, res, function(err){
+								if(err){
+									errback(err);
+								} else {
+									successback({userName:user.email});
+								}
+							});	
+						} else {
+							errback(new distro.error.ClientError("registration.errors.invalidCredentials"));
+						}
+					});
+				} else {
+					errback(new distro.error.ClientError("registration.errors.noCredentials"));
+				}
+			}));
+			app.get('/logout', methodNotAllowed);
+			app.post('/logout', distro.request.handleRequest(true, function(session, req, res, successback, errback){
+				global.sessions.endSession(session.sessionID, res, function(err){
+					if (err) {
+						errback(err);
 					} else {
-						errback(new distro.error.ClientError("registration.errors.noCredentials"));
+						successback({userName:null});
 					}
-				}));
-				app.get('/api/logout', methodNotAllowed);
-				app.post('/api/logout', distro.request.handleRequest(true, function(session, req, res, successback, errback){
-					global.sessions.endSession(session.sessionID, res, function(err){
+				});
+			}));
+			app.get('/register', methodNotAllowed);
+			app.post('/register', distro.request.handleRequest(false, function(session, req, res, successback, errback){
+				var body = req.body;
+				if(body && body.email && body.password){
+					global.users.registerUser(body.email, body.password, function(err, userID){
 						if (err) {
 							errback(err);
 						} else {
-							successback({userName:null});
+							global.sessions.startSessionForUserID(userID, null, req, res, function(err){
+								if(err){
+									errback(err);
+								} else {
+									successback({userName: body.email});
+								}
+							});
 						}
 					});
-				}));
-				app.get('/api/register', methodNotAllowed);
-				app.post('/api/register', distro.request.handleRequest(false, function(session, req, res, successback, errback){
-					var body = req.body;
-					if(body && body.email && body.password){
-						global.users.registerUser(body.email, body.password, function(err, userID){
-							if (err) {
-								errback(err);
-							} else {
-								global.sessions.startSessionForUserID(userID, null, req, res, function(err){
-									if(err){
-										errback(err);
-									} else {
-										successback({userName: body.email});
-									}
-								});
-							}
-						});
+				} else {
+					errback(new distro.error.ClientError("registration.errors.noCredentials"));
+				}
+			}));
+			app.get('/ping', distro.request.handleRequest(true, function(session, req, res, successback, errback){
+				successback();
+			}));
+			app.get('/:bandID', distro.request.handleRequest(false, function(session, req, res, successback, errback){
+				global.bands.findBandByID(req.params.bandID, function(err, bandDoc){
+					if(err){
+						errback(err);
+					} else if(JSON.stringify(bandDoc)){
+						successback(bandDoc);
 					} else {
-						errback(new distro.error.ClientError("registration.errors.noCredentials"));
+						errback(new distro.error.ClientError("bands.errors.noBand"));
 					}
-				}));
-				app.get('/api/ping', distro.request.handleRequest(true, function(session, req, res, successback, errback){
-					successback();
-				}));
-				app.get('/api/:bandID', distro.request.handleRequest(false, function(session, req, res, successback, errback){
-					global.bands.findBandByID(req.params.bandID, function(err, bandDoc){
-						if(err){
-							errback(err);
-						} else if(JSON.stringify(bandDoc)){
-							successback(bandDoc);
-						} else {
-							errback(new distro.error.ClientError("bands.errors.noBand"));
-						}
-					});
-				}));
-			}),
-			connect.staticProvider(__dirname + '/static')
-		).listen(3000);
+				});
+			}));
+		}))
+		/*.use('/', connect.router())*/
+		.listen(3000);
 		util.log('Alive on port 3000');
 	});
 });
