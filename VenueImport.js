ab891b78d8fc4efd2978d91bba2808297e545d3e
Sam Epstein <same008@gmail.com>
2011-02-01 07:05:43 -0500

    More better importing
    Signed-off-by: Sam Epstein <same008@gmail.com>

--- VenueImport.js
+++ VenueImport.js
@@ -8,8 +8,8 @@ exportDB = new mongoDB.Db('Distro', new mongoDB.Server(process.env['MONGO_NODE_D
 
 importDB.open(function(err, db) {
 	exportDB.open(function(err, exportDB){
-		exportDB.collection('bands', function(err, exportColl){
-			db.collection('venue', function(err, coll){
+		exportDB.collection('networks', function(err, exportColl){
+			db.collection('import', function(err, coll){
 				coll.find(function(err, cursor){
 					var counter = 1;
 					(function nextRecord(){
@@ -19,23 +19,23 @@ importDB.open(function(err, db) {
 									  urlKeyList = ['FOURSQUARE', 'HOME_PAGE', 'FACEBOOK', 'MYSPACE', 'TWITTER', 'YELP', 'GOOGLE_MAP', 'FLICKR_STREAM', 'YOUTUBE', 'REVERB_NATION', 
 													'CAL_GOOG', 'CAL_MAIN', 'PHOTO_LINK', 'LASTFM', 'PANDORA', 'SOUNDCLOUD', 'LINKEDIN', 'ILIKE', 'ITUNES', 'VIMEO', 'BANDCAMP', 'BLOG', 'GIGMAVEN', 'ARCHIVE', 'JAMBASE'], 
 
-									basicRenameList = [{oldName:"TYPE", newName:"type"}, {oldName:"NETWORK_NAME", newName:"name"}, {oldName:"FULL_NAME", newName:"fullname"}, {oldName:"STREET_ADDRESS", newName:"streetAddress"},
-													{oldName:"CITY, STATE", newName:"citystate"}, {oldName:"COUNTRY", newName:"country"}, {oldName:"HOME_PAGE", newName:"homepage", object:"presence"},
+									basicRenameList = [{oldName:"TYPE", newName:"type"}, {oldName:"NETWORK_NAME", newName:"name"}, {oldName:"FULL_NAME", newName:"fullname"}, {oldName:"STREET_ADDRESS", newName:"streetAddress", object:"location"},
+													{oldName:"CITY, STATE", newName:"citystate", object:"location"}, {oldName:"COUNTRY", newName:"country", object:"location"}, {oldName:"HOME_PAGE", newName:"homepage", object:"presence"},
 													{oldName:"EMAIL_VENUE_BOOKING", newName:"booking", object:"email"}, {oldName:"EMAIL_GENERAL", newName:"general", object:"email"}, {oldName:"EMAIL_US", newName:"usbooking", object:"email"}, 
-													{oldName:"EMAIL_EU", newName:"eubooking", object:"email"}, {oldName:"EMAIL_PRESS", newName:"press", object:"email"}, {oldName:"EMAIL_MANAGER", newName:"manager", object:"email"}
+													{oldName:"EMAIL_EU", newName:"eubooking", object:"email"}, {oldName:"EMAIL_PRESS", newName:"press", object:"email"}, {oldName:"EMAIL_MANAGER", newName:"manager", object:"email"},
 													{oldName:"PHONE", newName:"phone"}, {oldName:"ZIP", newName:"zip"}, {oldName:"PHOTO_BY", newName:"photoCred"}, {oldName:"CAL_MAIN", newName:"calendar"}, 
 													{oldName:"CAL_GOOG", newName:"calendarGoogle"}],
 
 									urlPathList = [{oldName:"FOURSQUARE", newName:"foursquare", object:"presence"}, {oldName:"FACEBOOK", newName:"facebook", object:"presence"}, {oldName:"TWITTER", newName:"twitter", object:"presence"}, 
 												{oldName:"MYSPACE", newName:"myspace", object:"presence"}, {oldName:"LASTFM", newName:"lastfm", object:"presence"}, {oldName:"PANDORA", newName:"pandora", object:"presence"}, 
-												{oldName:"SOUNDCLOUD", newName:"soundcloud", object:"presence"}, ];
+												{oldName:"SOUNDCLOUD", newName:"soundcloud", object:"presence"}, {oldName:"PHOTO_LINK", newName:"photoCredURL"}, {oldName:"ILIKE", newName:"ilike", object:"presence"},
+												{oldName:"VIMEO", newName:"vimeo", object:"presence"}, {oldName:"GIGMAVEN", newName:"gigmaven", object:"presence"}, {oldName:"ARCHIVE", newName:"archive", object:"presence"}, 
+												{oldName:"JAMBASE", newName:"jambase", object:"presence"}, {oldName:"REVERB_NATION", newName:"reverbnation", object:"presence"}, {oldName:"YELP", newName:"yelp", object:"presence"}];
 
 								record = doc; //This could just be record = {}; but I'm leaving it like this for now
 								record.presence = {};
-								record.presence.email = {};
 								record.location = {};
 								record.Timestamp = new Date(doc.Timestamp);
-								record.fullname = doc['Band Name'];
 								function httpize(url){
 									if(/^[hH][tT][tT][pP][sS]?:\/\/(.*)/.exec(url)){
 										return url;
@@ -61,10 +61,10 @@ importDB.open(function(err, db) {
 											//util.log("httpizing " + doc[key] + " \n \t \t - Result: " + httpize(doc[key]));
 											doc[key] = httpize(doc[key]);
 										}
-										var killTrailingSlash = /(.*)\/$/.exec(doc[key]);
-										if(killTrailingSlash){
-											doc[key] = killTrailingSlash[1];
-										}
+										//var killTrailingSlash = /(.*)\/$/.exec(doc[key]);
+										/*if(killTrailingSlash){*/
+											//doc[key] = killTrailingSlash[1];
+										/*}*/
 									}
 								}
 								function basicRename(acceptObj){
@@ -102,7 +102,8 @@ importDB.open(function(err, db) {
 									delete record[oldRecordName];
 								  }
 								}
-								
+							_.each(basicRenameList, basicRename);
+							_.each(urlPathList, urlPathnameRename);
 								// record.myspace = myspaceRegEx.exec(doc['Myspace URL']);
 								//record.name = doc['^NETWORKname^'].replace(/\^/g, "");
 								//delete record['Band Name'];
