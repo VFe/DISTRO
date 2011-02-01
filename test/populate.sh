4cdbfad20824e189f5cb6508a4a4c81afc1e4c1a
Sidney San Martín <s@sidneysm.com>
2011-02-01 14:32:16 -0500

    Add track filenames to test data.

--- test/populate.sh
+++ test/populate.sh
@@ -12,7 +12,7 @@ ARE YOU SURE YOU WANT TO DO THIS? [yN]: " proceed;
 [[ $proceed != [yY] ]] && exit;
 
 mongoimport -d "${database:-Distro}" -c "networks" --drop <<'EOF'
-{ "_id" : { "$oid" : "000000000000000000000000" }, "name" : "network_1" }
+{ "_id" : { "$oid" : "000000000000000000000000" }, "name" : "test_network_1" }
 { "_id" : { "$oid" : "000000000000000000000001" }, "name" : "bklyn", "fullName" : "BKLYN Lounge" }
 EOF
 
@@ -24,12 +24,12 @@ EOF
 
 mongoimport -d "${database:-Distro}" -c "tracks" --drop <<'EOF'
 { "_id" : { "$oid" : "4d22380a704f8f211756ca31" }, "name" : "Released before subscription (FAIL)", "release" : { "$date" : 1259625600000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
-{ "_id" : { "$oid" : "4d22380a704f8f211756ca32" }, "name" : "On deck (PASS)", "release" : { "$date" : 1259625600000 }, "onDeck" : [ { "start" : { "$date" : 1259625600000 } } ], "network" : { "$oid" : "000000000000000000000000" } }
+{ "_id" : { "$oid" : "4d22380a704f8f211756ca32" }, "name" : "Chez (On deck)", "filename" : "Chez", "release" : { "$date" : 1259625600000 }, "onDeck" : [ { "start" : { "$date" : 1259625600000 } } ], "network" : { "$oid" : "000000000000000000000000" } }
 { "_id" : { "$oid" : "4d22380a704f8f211756ca33" }, "name" : "On deck in the past (FAIL)", "release" : { "$date" : 1259625600000 }, "onDeck" : [ { "start" : { "$date" : 1259625600000 }, "end" : { "$date" : 1259971200000 } } ], "network" : { "$oid" : "000000000000000000000000" } }
 { "_id" : { "$oid" : "4d22380a704f8f211756ca34" }, "name" : "Released in the future (FAIL)", "release" : { "$date" : 32532537600000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
-{ "_id" : { "$oid" : "4d22380a704f8f211756ca35" }, "name" : "Released during first subscription (PASS)", "release" : { "$date" : 1264982400000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
+{ "_id" : { "$oid" : "4d22380a704f8f211756ca35" }, "name" : "Crafty (Released during first subscription)", "filename" : "Crafty", "release" : { "$date" : 1264982400000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
 { "_id" : { "$oid" : "4d22380a704f8f211756ca36" }, "name" : "Released between subscriptions (FAIL)", "release" : { "$date" : 1277942400000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
-{ "_id" : { "$oid" : "4d22380a704f8f211756ca37" }, "name" : "Released during second subscription (PASS)", "release" : { "$date" : 1285891200000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
+{ "_id" : { "$oid" : "4d22380a704f8f211756ca37" }, "name" : "Jay-Z (Released during second subscription)", "filename" : "Jay-Z", "release" : { "$date" : 1285891200000 }, "onDeck" : [], "network" : { "$oid" : "000000000000000000000000" } }
 EOF
 
 mongoimport -d "${database:-Distro}" -c "users" --drop <<'EOF'
