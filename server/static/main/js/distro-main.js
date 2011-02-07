a4ac4f726848002a80caf957cd806a7e879acfed
Sam Epstein <same008@gmail.com>
2011-02-07 17:23:52 -0500

    First pass on map link
    Signed-off-by: Sam Epstein <same008@gmail.com>

--- server/static/main/js/distro-main.js
+++ server/static/main/js/distro-main.js
@@ -459,10 +459,11 @@ distro.loadLandingPage = function(name, callback){
 									["%p", {style:"margin-top:0px; margin-right: 0.25em; margin-bottom: 0px; margin-left:0px; text-align: right; float:right;"}, "Photo by ",
 										["%a",{target:"_blank", href:{ key: "photoCredURL"}, style:"text-decoration:none;"}, { key: "photoCred"}]
 									],
-									["%a#location", {href: {key: "map"}}, {key: "location", template:[
+									["#location", {key: "location", template:[
 										["%p",{style:"margin-top: 0.25em; margin-right: 0em; margin-bottom: 0em; margin-left: 0em;"}, { key: "citystate"}],
 										["%p",{style:"margin-top:0px;"}, { key: "country"}]
-									]}]
+									]}, 
+									{key:"map", conditional: ["a#mapLink", {href:{key:"map"}}]}]
 								],
 								["%span#artist",{style:"font-size:36px;"},
 									["%p",{style:"margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px;"}, { key: "fullname"}]
