eb7e08140d16e8e0aeba43fc406cfeaf0c0328e4
Sam Epstein <same008@gmail.com>
2011-02-08 18:54:47 -0500

    Little server to run script to update
    Signed-off-by: Sam Epstein <same008@gmail.com>

---
+++ kyledb.js
@@ -0,0 +1,25 @@
+var http = require('http'),
+	exec = require('child_process').exec,
+	fs = require('fs'),
+	auth = fs.readFileSync("/auth.txt", encoding = 'utf8');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	exec("bash /loadNetworks.sh", [], {cwd:"/", env:auth}, 
+	 function(error, stdout, stdin){
+		res.write("STDOUT: "+stdout);
+		if(error){
+			res.write("Error: "+error);
+		}
+		res.end("done"+auth);
+		console.log("Wrote some shit I hope");
+	});
+//	script.stdout.on('data', function(data){
+//		res.write(data, encoding='utf8');
+//	});
+//	script.on('exit', function(code){
+//		console.log("Done");
+//		res.end("Exited with code: "+code, encoding='utf8');
+//	});
+}).listen(8118, "127.0.0.1");
+
