e5420d9e48f596be670b2105e100e88dc620cb07
Sam Epstein <same008@gmail.com>
2011-03-07 23:09:58 -0500

    Added Google Analytics Code
    Signed-off-by: Sam Epstein <same008@gmail.com>

--- server/static/main/js/distro-main.js
+++ server/static/main/js/distro-main.js
@@ -852,5 +852,14 @@ distro.init(function(){
 			return original.apply(this, arguments);
 		};
 	})(jQuery);
-
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-21896928-1']);
+	_gaq.push(['_setDomainName', '.distro.fm']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 });
