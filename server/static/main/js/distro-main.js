VFe/DISTRO

commit 20c4137fb9228997179dd215fc4e53024867236a

Fix the case of the overlapping tutorial stages.
When a user visited DISTRO for the first time, from a network URL,
and subscribed without doing anything else, both the "new music" and
"find networks" tutorial bubbles would appear. This is fixed, by making
distro.tutorial.show wait to show a stage until the current one has been
hidden.

server/static/main/js/distro-main.js
@@ -83,7 +83,10 @@ distro.tutorial = {
 		}
 	},
 	show: function(stage){
-		if (this.shouldShow(stage)) {
+		if (this.current) {
+			this.next = stage;
+		} else if (this.shouldShow(stage)) {
+			this.current = stage;
 			this.stages[stage].show.apply(this.stages[stage], Array.prototype.slice.call(arguments, 1));
 		}
 	},
@@ -94,6 +97,13 @@ distro.tutorial = {
 			$element.fadeOut(function(){
 				$(this).remove();
 			});
+		}
+		if (this.current === stage) {
+			delete this.current;
+		}
+		if (this.next) {
+			this.show(this.next);
+			delete this.next;
 		}
 	},
 	stages: {
@@ -875,8 +885,8 @@ distro.loadLandingPage = function(name, callback){
 								success: function(){
 									subscribed = true;
 									$subscribeButton.addClass('disabled');
-									distro.lightbox.pop();
 									distro.tutorial.show('newMusic', model);
+									distro.lightbox.pop();
 								}
 							});
 						}
@@ -889,6 +899,9 @@ distro.loadLandingPage = function(name, callback){
 							}, 800);
 						}
 					}
+				},
+				hide: function(){
+					distro.tutorial.hide('subscribe');
 				}
 			});
 			callback(model);
@@ -1052,6 +1065,7 @@ distro.Router = Backbone.Controller.extend({
 			},
 			hide: function(){
 				$(window.document).unbind('keypress', keypressHandler);
+				distro.tutorial.hide('search');
 			}
 		});
 	},

commit 46e13e68fa91b63c9b02138749b26378eefe4857

Fix some tutorial stages broken since 20c4137fb9228997179dd215fc4e53024867236a.

server/static/main/js/distro-main.js
@@ -83,11 +83,12 @@ distro.tutorial = {
 		}
 	},
 	show: function(stage){
+		var args = Array.prototype.slice.call(arguments, 1);
 		if (this.current) {
-			this.next = stage;
+			this.next = { stage: stage, arguments: args };
 		} else if (this.shouldShow(stage)) {
 			this.current = stage;
-			this.stages[stage].show.apply(this.stages[stage], Array.prototype.slice.call(arguments, 1));
+			this.stages[stage].show.apply(this.stages[stage], args);
 		}
 	},
 	hide: function(stage){
@@ -102,7 +103,7 @@ distro.tutorial = {
 			delete this.current;
 		}
 		if (this.next) {
-			this.show(this.next);
+			this.show.apply(this, [this.next.stage].concat(this.next.arguments));
 			delete this.next;
 		}
 	},