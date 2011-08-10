aff3a15038b355f470976770d28e16c381f1ceb6
Sam Epstein <same008@gmail.com>
2011-08-09 21:55:18 -0400

    Slight refactoring

--- server/lib/distro/transcode.sh
+++ server/lib/distro/transcode.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [[ $IS_WAV && $INPUT_FILE && $OUTPUT_FILE ]]
+if [[ $INPUT_FILE && $OUTPUT_FILE ]]
 then
 	lame --silent -V3 "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
-elif [[ $INPUT_FILE && $OUTPUT_FILE ]]
-then
-	lame --silent -V3 "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
-#	id3cp -wn "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
 else
 	exit 1
 fi
+# if [[ $IS_WAV ]]; then
+# 	id3cp -wn "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
+# fi
 
 echo "Input: $INPUT_FILE"
 echo "Output: $OUTPUT_FILE"
