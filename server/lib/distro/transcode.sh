fb530d7e17e1489c7d87e4ff50f99327de410e12
Sam Epstein <same008@gmail.com>
2011-08-09 21:55:05 -0400

    Quiet down lame

--- server/lib/distro/transcode.sh
+++ server/lib/distro/transcode.sh
@@ -2,10 +2,10 @@
 
 if [[ $IS_WAV && $INPUT_FILE && $OUTPUT_FILE ]]
 then
-	lame -V3 "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
+	lame --silent -V3 "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
 elif [[ $INPUT_FILE && $OUTPUT_FILE ]]
 then
-	lame -V3 "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
+	lame --silent -V3 "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
 #	id3cp -wn "$INPUT_FILE" "$OUTPUT_FILE" || exit $?
 else
 	exit 1
