25b2f5a9fc5ba4ba674e73d65b40fdc37d1b5277
Sidney San Martín <s@sidneysm.com>
2011-09-27 23:42:35 -0400

    Remove the need to hardcode all of distro's submodules

--- server/lib/distro/index.js
+++ server/lib/distro/index.js
@@ -1,11 +1,5 @@
-[
-	'request',
-	'error',
-	'Users',
-	'Sessions',
-	'Tracks',
-	'Networks',
-	'init',
-].forEach(function(component){
-	exports[component] = require('./' + component);
+require('fs').readdirSync(__dirname).forEach(function(file){
+	if (file !== 'index.js' && file.substr(-3) === '.js') {
+		exports[file.substring(0, file.length - 3)] = require(__dirname + '/' + file);
+	}
 });
